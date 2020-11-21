c5c3bfc44d7a34c2a153e03f2a4063150ee2eb49
frnietz <57188938+frnietz@users.noreply.github.com>
2020-11-21 07:53:53 +0200

    Update and rename setup.sh to create_config.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/echo “\
-[general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.tomlecho “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
